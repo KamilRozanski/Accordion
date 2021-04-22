488a3a54356e71adbcd9ddbfcee335b15b4fc0c9
KamilRozanski <motomc1@gmail.com>
2021-04-22 12:25:39 +0100

    1 file modified

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const nonActiveBtn = btns[0].innerHTML;
 const header = document.querySelectorAll(".header");
 const span = document.querySelectorAll(".span");
 const background = document.querySelectorAll(".background");
+const content = document.querySelectorAll(".content");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
@@ -14,14 +15,10 @@ for (let i = 0; i < btns.length; i++) {
 
     if (content != null) {
       if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-        btns[i].innerHTML = nonActiveBtn;
-        btns[i].classList.remove("bg-indigo");
-        btns[i].classList.add("border-grey");
-        span[i].classList.remove("text-indigo");
-        background[i].classList.remove("bg-grey-lightest", "border-indigo");
+        hidePanels();
       } else {
-        content.style.maxHeight = content.scrollHeight + "px";
+        showPanel(content);
+
         btns[i].innerHTML = actBtn;
         btns[i].classList.add("bg-indigo");
         span[i].classList.add("text-indigo");
@@ -30,4 +27,20 @@ for (let i = 0; i < btns.length; i++) {
       }
     }
   });
+}
+function showPanel(el) {
+  hidePanels();
+  el.style.maxHeight = el.scrollHeight + "px";
+}
+
+// Function to Hide all shown Panels
+function hidePanels() {
+  for (let i = 0; i < content.length; i++) {
+    content[i].style.maxHeight = null;
+    btns[i].innerHTML = nonActiveBtn;
+    btns[i].classList.remove("bg-indigo");
+    btns[i].classList.add("border-grey");
+    span[i].classList.remove("text-indigo");
+    background[i].classList.remove("bg-grey-lightest", "border-indigo");
+  }
 }
